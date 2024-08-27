2bedf3a3810ebdf3cb5236bcf1bcb31c437880e8
Ayo89 <ayose89@gmail.com>
2024-08-27 23:30:40 +0530

    Create input and button for custom boxs

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-let boxWidth = 14.70;
-let boxHeight = 14.70; 
+let boxWidth = 16;
+let boxHeight = 16;
 
-let containerWidth = (boxWidth * boxHeight) + 60;
-
-
+let body = document.querySelectorAll("body")[0];
 let container = document.querySelectorAll("#container")[0];
-container.style.width = containerWidth + 'px'
 
-console.log(container);
-for (let i = 0; i < 16; i++) {
-  for (let y = 0; y < 16; y++) {
-    let box = document.createElement("div");
-    box.style.width = boxWidth + 'px';
-    box.style.height = boxHeight + 'px';
+let form = document.createElement("form");
+body.insertBefore(form, container);
 
-    box.setAttribute("class", "box");
-    container.appendChild(box);
+let input = document.createElement("input");
+let button = document.createElement("button");
+button.textContent = "CREATE";
+
+let functionCreateBox = (numberBox) => {
+
+  for (let i = 0; i < numberBox; i++) {
+    for (let y = 0; y < numberBox; y++) {
+      let box = document.createElement("div");
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "red";
+      });
+      box.addEventListener("mouseout", () => {
+        box.style.backgroundColor = "white";
+      });
+      box.style.width = (boxWidth - 2.5) + "px";
+      box.style.height = (boxHeight - 2.5) + "px";
+      box.setAttribute("class", "box");
+      container.appendChild(box);
+    }
+    container.style.width = boxWidth * numberBox + "px";
+    container.style.height = boxHeight * numberBox + "px";
   }
-}
+};
+input.addEventListener("change", () => {
+  console.log(input.value);
+    numberBox = input.value;
+    functionCreateBox(numberBox);
+});
+form.appendChild(input);
+form.appendChild(button);
